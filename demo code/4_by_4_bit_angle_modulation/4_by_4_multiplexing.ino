ebe337ed66ee3d712e9a8ebaf9a19f36c8d36156
Winswins <73000223+Winswins22@users.noreply.github.com>
2022-01-03 00:58:17 -0500

    Timer is function, basic BAM outlined

--- demo code/4_by_4_bit_angle_modulation/4_by_4_multiplexing.ino
+++ demo code/4_by_4_bit_angle_modulation/4_by_4_multiplexing.ino
@@ -3,6 +3,7 @@ unsigned char defeatTheCrumbyPreprocessor;
 
 // The Carry
 // https://www.kevindarrah.com/download/8x8x8/RGB_CubeV12_BitwiseFix.ino
+// https://www.youtube.com/watch?v=xmScytz9y0M
 
 #include <SPI.h>// SPI Library used to clock data out to the shift registers
 
@@ -14,8 +15,20 @@ unsigned char defeatTheCrumbyPreprocessor;
 //These variables are used by multiplexing and Bit Angle Modulation Code
 int shift_out;//used in the code a lot in for(i= type loops
 
+int level=0;//keeps track of which level we are shifting data to
+int anodelevel=0;//this increments through the anode levels
+int BAM_Bit, BAM_Counter=0; // Bit Angle Modulation variables to keep track of things
+
+// the data to send inside the BAM counter
+byte BAMDataToSend;
+
 // core of BAM, 4 bit resolution. Each LED is represented 4 times (aka 4-bit res).
-byte led0[2], led1[2], led2[2], led3[2];
+// the 4 most significant bits are ignored in each byte
+byte led0[4], led1[4], led2[4], led3[4];
+
+// Since this is a 4x4 instead of an 8x8x8, this is scaled down.
+// as a result, the 4 most significant bits are ignored in each byte
+byte anode[4];
 
 void setup() {
 
@@ -34,6 +47,11 @@ void setup() {
   TIMSK1 = B00000010;//bit 1 set to call the interrupt on an OCR1A match
   OCR1A=30;
 
+  anode[0]=0b00000001;
+  anode[1]=0b00000010;
+  anode[2]=0b00000100;
+  anode[3]=0b00001000;
+
   //set pins as output
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
@@ -45,10 +63,106 @@ void setup() {
 
 }
 
+// Using interrupts so no need to put anything here.
 void loop() {
 
 }
 
-void LED(int row, int column, byte red, byte green, byte blue){
-  
+// Heavily simplified LED function from the video.
+
+/** void LED(int column, byte brightness)
+  * 
+  * @param row, int. The index of the LED row. Supports 0-3. For getting a byte from the byte array. (aka led0, led1, led2, led3)
+  * @param column, int. The index of the led in the row to light up. Supports 0-3. For accessing a bit in a byte.
+  * @param brightness, byte. Controls bit angle modulation. Only the 4 least significant bits are used.
+  *
+  */
+void LED(int row, int column, byte brightness){
+
+  // writes a bit to led0: (row, col, boolean: turn on or off depending on BAM)
+  bitWrite(led0[row], column, bitRead(brightness, 0));
+  bitWrite(led1[row], column, bitRead(brightness, 1));
+  bitWrite(led2[row], column, bitRead(brightness, 2)); 
+  bitWrite(led3[row], column, bitRead(brightness, 3)); 
+
+}
+
+// gee golly hope this works!!!!!! (relies on registers)
+// The BAM timer.
+ISR(TIMER1_COMPA_vect){//***MultiPlex BAM***MultiPlex BAM***MultiPlex BAM***MultiPlex BAM***MultiPlex BAM***MultiPlex BAM***MultiPlex BAM
+
+  // sanity check
+  // Serial.print("yo this is actually working bruhhhhhhhhh\n");
+
+  // for calculating the brightness using BAM
+  // you can think of BAM_Bit as the brightness for BAM: on for 1 tick, off for 2, on for 4, on for 8, etc....
+  // no idea why the numbers are scuffed
+  if(BAM_Counter==8)
+  BAM_Bit++;
+  else
+  if(BAM_Counter==24)
+  BAM_Bit++;
+  else
+  if(BAM_Counter==56)
+  BAM_Bit++;
+
+  BAM_Counter++;//Here is where we increment the BAM counter
+
+// Here, we have to do some bitmath to send out both anode + cathode information all at once since
+// we need to send out 1 byte for SPI.transfer() to work. Since anode info is a total of 4 bits and cathode is
+// a total of 4 bits as well, we can combine both pieces of information to create a byte.
+// We wouldn't need to do this if we had an 8x8, but we dont so yea
+switch (BAM_Bit){
+case 0:
+ // level here refers to the z-axis. Since we dont have one, treat it as 0!
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ BAMDataToSend = 
+ SPI.transfer(BAMDataToSend);
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ BAMDataToSend = 
+ SPI.transfer(BAMDataToSend);
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ BAMDataToSend = 
+ SPI.transfer(BAMDataToSend);
+  break;
+case 1:
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(red1[shift_out]);
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(green1[shift_out]); 
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(blue1[shift_out]);
+  break;
+ case 2:
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(red2[shift_out]);
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(green2[shift_out]); 
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(blue2[shift_out]);
+ break;
+ case 3:
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(red3[shift_out]);
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(green3[shift_out]); 
+ for(shift_out=level; shift_out<level+8; shift_out++)
+ SPI.transfer(blue3[shift_out]);
+ //Here is where the BAM_Counter is reset back to 0, it's only 4 bit, but since each cycle takes 8 counts,
+ //, it goes 0 8 16 32, and when BAM_counter hits 64 we reset the BAM
+  if(BAM_Counter==120){
+  BAM_Counter=0;
+  BAM_Bit=0;
+  }
+  break;
+}//switch_case
+
+// we would send out anode info here if we had an 8x8x8
+// SPI.transfer(anode[anodelevel]);//finally, send out the anode level byte
+
+PORTD |= 1<<latch_pin;//Latch pin HIGH
+PORTD &= ~(1<<latch_pin);//Latch pin LOW
+
+
+
 }
